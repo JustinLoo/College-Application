e38a474593ba49591c75db444cfdca2b69a70887
JISPIX <36491619+JISPIX@users.noreply.github.com>
2021-03-05 10:32:15 -0800

    v2

--- college.py
+++ college.py
@@ -1,70 +1,239 @@
 import mysql.connector as mysql
-#import sql
 
+db = mysql.connect(host="localhost",user="root",password="",database="college")
+commandHandler = db.cursor(buffered=True)
 
-db = mysql.connect(host = "localhost", user = "root", password = "", database = "college")
-#connect to mySQL database. The password is left blank because root has no default password set at the moment
-commandHandler = db.cursor(buffered = True)
-#this makes it so that it will execute any query and SQL code
+print("___________________________________________________________________")
 
-#if it is admin:
-def admin():
-    print("Admin Menu")
-    print("1. Register new Student")
-    print("2. Register new Teacher")
-    print("3. Delete exsisting Student")
-    print("4. Delete exsisting Teacher")
-    print("5. Logout")
-
-    userOption = input(str("Option: "))
-    if userOption == "1":
-        print("Register a new Student!")
-        newStudent = input(str("set Student name: "))
-        studentPassword = input(str("set Student password: "))
-        queryValues = (newStudent, studentPassword)
-        commandHandler.execute("INSERT INTO users (username,password,prvilege) VALUES (%s,%s, 'student')",queryValues)
-        db.commit()
-        print(newStudent + "has been registered as a student")
-        print("_________________________________________________________")
+def teacher_session():
+    while 1:
+        print("___________________________________________________________________")
     
+        print("")
+        print("Teacher's Menu")
+        print("1. Mark student register")
+        print("2. View register")
+        print("3. Logout")
+        print("___________________________________________________________________")
+    
+        userInput = input(str("Option : "))
+        if userInput == "1":
+            print("")
+            print("Mark student register")
+            commandHandler.execute("SELECT username FROM users WHERE privilege = 'student'")
+            records = commandHandler.fetchall()
+            date    = input(str("Date : DD/MM/YYYY : "))
+            for record in records:
+                record = str(record).replace("'","")
+                record = str(record).replace(",","")
+                record = str(record).replace("(","")
+                record = str(record).replace(")","")
+                #Present | Absent | Late
+                status = input(str("Status for " + str(record) + "P/A/L : "))
+                queryValues = (str(record),date,status)
+                commandHandler.execute("INSERT INTO attendance (username, date, status) VALUES(%s,%s,%s)",queryValues)
+                db.commit()
+                print(record + " Marked as " + status)
+        elif userInput == "2":
+            print("")
+            print("Viewing all student registers")
+            commandHandler.execute("SELECT username, date, status FROM attendance")
+            records = commandHandler.fetchall()
+            print("Displaying all registers")
+            for record in records:
+                print(record)
+            print("___________________________________________________________________")
+        elif userInput == "3":
+            break
+            print("___________________________________________________________________")
+        else:
+            print("No valid option was selected")
+            print("___________________________________________________________________")
+
+def student_session(username):
+    while 1:
+        print("___________________________________________________________________")
+    
+        print("")
+        print("Student's Menu")
+        print("")
+        print("1. View Register")
+        print("2. Download Register")
+        print("3. Logout")
+        print("___________________________________________________________________")
+
+        userInput = input(str("Option : "))
+        if userInput == "1":
+            print("Displaying register")
+            username = (str(username),)
+            commandHandler.execute("SELECT date, username, status FROM attendance WHERE username = %s",username)
+            records = commandHandler.fetchall()
+            for record in records:
+                print(record)
+            print("___________________________________________________________________")
+        elif userInput == "2":
+            print("Downloading Register")
+            username = (str(username),)
+            commandHandler.execute("SELECT date, username, status FROM attendance WHERE username = %s",username)
+            records = commandHandler.fetchall()
+            for record in records:
+                with open("C:/Users/Johan Godinho/Desktop/register.txt", "w") as f:
+                    f.write(str(records)+"\n")
+                f.close()
+            print("All records saved")
+            print("___________________________________________________________________")
+        elif userInput == "3":
+            break
+            print("___________________________________________________________________")
+        else:
+            print("No valid option was selected")
+            print("___________________________________________________________________")
 
 
-#checks if credentials are corerect
-def authAdmin():
+def admin_session():
+    while 1:
+        print("___________________________________________________________________")
+    
+        print("")
+        print("Admin Menu")
+        print("1. Register new Student")
+        print("2. Register new Teacher")
+        print("3. Delete Existing Student")
+        print("4. Delete Existing Teacher")
+        print("5. Logout")
+        print("___________________________________________________________________")
+
+        userInput = input(str("Option : "))
+        if userInput == "1":
+            print("")
+            print("Register New Student")
+            username = input(str("Student username : "))
+            password = input(str("Student password : "))
+            queryValues = (username,password)
+            commandHandler.execute("INSERT INTO users (username,password,privilege) VALUES (%s,%s,'student')",queryValues)
+            db.commit()
+            print(username + " has been registered as a student")
+            print("___________________________________________________________________")
+        
+        elif userInput == "2":
+            print("")
+            print("Register New Teacher")
+            username = input(str("Teacher username : "))
+            password = input(str("Teacher password : "))
+            queryValues = (username,password)
+            commandHandler.execute("INSERT INTO users (username,password,privilege) VALUES (%s,%s,'teacher')",queryValues)
+            db.commit()
+            print(username + " has been registered as a teacher")
+            print("___________________________________________________________________")
+    
+        elif userInput == "3":
+            print("")
+            print("Delete Existing Student Account")
+            username = input(str("Student username : "))
+            queryValues = (username,"student")
+            commandHandler.execute("DELETE FROM users WHERE username = %s AND privilege = %s ",queryValues)
+            db.commit()
+            
+            if commandHandler.rowcount < 1:
+                print("User not found")
+                print("___________________________________________________________________")
+            else:
+                print(username + " has been deleted")
+                print("___________________________________________________________________")
+            print("___________________________________________________________________")
+
+        elif userInput == "4":
+            print("")
+            print("Delete Existing Teacher Account")
+            username = input(str("Teacher username : "))
+            queryValues = (username,"teacher")
+            commandHandler.execute("DELETE FROM users WHERE username = %s AND privilege = %s ",queryValues)
+            db.commit()
+            if commandHandler.rowcount < 1:
+                print("User not found")
+                print("___________________________________________________________________")
+            else:
+                print(username + " has been deleted")
+                print("___________________________________________________________________")
+            print("___________________________________________________________________")
+
+        elif userInput == "5":
+            break
+            print("___________________________________________________________________")
+        else:
+            print("No valid option selected")
+            print("___________________________________________________________________")
+
+def studentAuth():
+    print("___________________________________________________________________")
+    
+    print("")
+    print("Student's Login")
+    print("")
+    username = input(str("Username : "))
+    password = input(str("Password : "))
+    queryValues = (username, password, "student")
+    commandHandler.execute("SELECT username FROM users WHERE username = %s AND password = %s AND privilege = %s",queryValues)
+    if commandHandler.rowcount <= 0:
+        print("Invalid login details")
+        print("___________________________________________________________________")
+    else:
+        student_session(username)
+        print("___________________________________________________________________")
+def teacherAuth():
+    print("___________________________________________________________________")
+    
+    print("")
+    print("Teacher's Login")
+    print("")
+    username = input(str("Username : "))
+    password = input(str("Password : "))
+    queryValues = (username, password)
+    commandHandler.execute("SELECT * FROM users WHERE username = %s AND password = %s AND privilege = 'teacher'",queryValues)
+    if commandHandler.rowcount <= 0:
+        print("Login not recognized")
+        print("___________________________________________________________________")
+    else:
+        teacher_session()
+        print("___________________________________________________________________")
+
+def adminAuth():
+    print("___________________________________________________________________")
+    
     print("")
     print("Admin Login")
     print("")
-    username = input(str("Username: "))
-    password = input(str("Password: "))
+    username = input(str("Username : "))
+    password = input(str("Password : "))
     if username == "admin":
         if password == "password":
-            admin()
+            admin_session()
         else:
-            print("Wrong pasword")
+            print("Incorrect password !")
+            print("___________________________________________________________________")
     else:
-        print("Login details are wrong")
+        print("Login details not recognised") 
+        print("___________________________________________________________________")
 
-#main loop executes all the startup
 def main():
-    while True:
-        print("Welcome to my college application")
+    while 1:
+        print("___________________________________________________________________")
+    
+        print("Welcome to the college system")
         print("")
-        print("1. Log in as a student")
-        print("2. Log in as a teacher")
-        print("3. Log in as a admin")
+        print("1. Login as student")
+        print("2. Login as teacher")
+        print("3. Login as admin")
+        print("___________________________________________________________________")
 
-        userInput = input(str("Option:"))
+        userInput = input(str("Option : "))
         if userInput == "1":
-            print("You have logged in as a student")
+            studentAuth()
         elif userInput == "2":
-            print("You have logged in as a teacher")
+            teacherAuth()
         elif userInput == "3":
-            
-            authAdmin()
+            adminAuth()
         else:
-            print("This is not a valid input")
-        
-
-
+            print("No valid option was selected")
 
 main()
